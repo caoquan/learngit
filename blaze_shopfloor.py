ad1e8b90ee97c1c22d5241e7fd0639b5e13b7ac4
jack cao <jack.cao2@quantacn.com>
2015-04-29 16:00:11 +0800

    change blaze_shopfloor.py with athour

--- blaze_shopfloor.py
+++ blaze_shopfloor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the LICENSE file. jack test
 
 """Blaze shopfloor proxy implementation.
 
