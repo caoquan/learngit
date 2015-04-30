fe31e00f56aee179276ba37d83504a8d2ffd6838
caoquan <jack.cao2@quantacn.com>
2015-04-30 12:55:11 +0800

    Update shopfloor_server.py
    add nothing

--- shopfloor_server.py
+++ shopfloor_server.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
+# gd
 
 '''
 This file starts a server for factory shop floor system.
